c0a21eeddc3cfc57cfeafa6b96671afc2f9eb05e
Multitallented <multitallented@pacbell.net>
2012-02-28 08:52:14 -0800

    added new CommandEvent for effects that want to listen for commands using /hs as a base

---
+++ src/multitallented/redcastlemedia/bukkit/herostronghold/events/CommandEffectEvent.java
@@ -0,0 +1,40 @@
+package multitallented.redcastlemedia.bukkit.herostronghold.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ *
+ * @author Multitallented
+ */
+public class CommandEffectEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Player player;
+    private final String[] args;
+
+    public CommandEffectEvent(String[] args, Player player) {
+        super("CommandEffectEvent");
+        this.args=args;
+        this.player = player;
+        
+    }
+    
+    public Player getPlayer() {
+        return player;
+    }
+    
+    public String[] getArgs() {
+        return args;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+}
