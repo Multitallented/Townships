4e99088f864e15085fcb2288eb6f1dc57b21b964
Daniel Miller <dmiller@anaheimoc.org>
2012-06-12 11:05:09 -0700

    Added documentation and potential fixes for various upkeep bugs

--- src/multitallented/redcastlemedia/bukkit/herostronghold/effect/Effect.java
+++ src/multitallented/redcastlemedia/bukkit/herostronghold/effect/Effect.java
@@ -61,6 +61,17 @@ public class Effect {
         return rm.getRegion(loc);
     }
     
+    /**
+     * Use this to find if a region (not super-region) has an effect and what 
+     * number is associated with that effect. The number has no internal meaning
+     * to HeroStronghold, only you decide what that number means. Example:
+     * arrowturrets use the number to determine arrow velocity. Conveyors use the
+     * number as the item id that needs to be moved.
+     * 
+     * @param effects regionManager.getRegionType(region.getType()).getEffects()
+     * @param name example: "denyblockbreak"
+     * @return the number following the effect (returns 0 if doesn't have that effect)
+     */
     public int regionHasEffect(ArrayList<String> effects, String name) {
         int data = 0;
         if (effects == null || effects.isEmpty())
@@ -76,11 +87,71 @@ public class Effect {
             return 0;
         return data;
     }
-    public boolean isOwnerOfRegion(Player p, Location l) {
+    
+    /**
+     * Use this to find if a region (not super-region) has an effect and what 
+     * number is associated with that effect. The number has no internal meaning
+     * to HeroStronghold, only you decide what that number means. Example:
+     * arrowturrets use the number to determine arrow velocity. Conveyors use the
+     * number as the item id that needs to be moved.
+     * 
+     * @param region the region to check
+     * @param name example: "denyblockbreak"
+     * @return the number following the effect (returns 0 if doesn't have that effect)
+     */
+    public int regionHasEffect(Region r, String name) {
+        int data = 0;
+        ArrayList<String> effects = plugin.getRegionManager().getRegionType(r.getType()).getEffects();
+        if (effects == null || effects.isEmpty())
+            return 0;
+        
+        for (String effect : effects) {
+            String[] params = effect.split("\\.");
+            if (params.length > 1 && params[0].equalsIgnoreCase(name)) {
+                data = Integer.parseInt(params[1]);
+            }
+        }
+        if (data < 1)
+            return 0;
+        return data;
+    }
+    
+    /**
+     * Checks if the player is an owner of the region. Equivalent to
+     * region.isOwner(player.getName());
+     * 
+     * @param player
+     * @param region
+     * @return 
+     */
+    public boolean isOwnerOfRegion(Player p, Region r) {
+        return r.isOwner(p.getName());
+    }
+    
+    /**
+     * Checks if the player is is a member of the region at the location
+     * specified. This will throw a NullPointerException if there is no region
+     * at that location.
+     * 
+     * @param player
+     * @param location the location of the region center region.getLocation()
+     * @return 
+     */
+    public boolean isOwnerOfRegion(Player p, Location l) throws NullPointerException {
         return getPlugin().getRegionManager().getRegion(l).getOwners().contains(p.getName());
     }
     
-    public boolean isMemberOfRegion(Player p, Location l) {
+    /**
+     * This function does more than just region.isMember(player.getName()). It
+     * checks if the region has added a super-region that the player is a member
+     * of. Also checks if the region has added "all" players.
+     * 
+     * @exception thows NPE if no region found at the given location
+     * @param player
+     * @param location the location of the region center region.getLocation()
+     * @return 
+     */
+    public boolean isMemberOfRegion(Player p, Location l) throws NullPointerException {
         RegionManager rm = getPlugin().getRegionManager();
         Region r = rm.getRegion(l);
         if (r.isMember(p.getName())) {
@@ -101,6 +172,13 @@ public class Effect {
         return false;
     }
     
+    /**
+     * Checks if the region has output and the chest is not full first. Then it
+     * checks if the chest has the necessary reagents.
+     * 
+     * @param location the location of the center of the region region.getLocation()
+     * @return 
+     */
     public boolean hasReagents(Location l) {
         try {
             if (l.getBlock().getState() == null) {
@@ -143,47 +221,69 @@ public class Effect {
         return false;
     }
     
+    /**
+     * Forces the region to consume upkeep items and output money and items
+     * regardless of the upkeep-chance.
+     * 
+     * @param location the location of the center of the region region.getLocation()
+     */
     public void forceUpkeep(Location l) {
         RegionManager rm = getPlugin().getRegionManager();
         Region r = rm.getRegion(l);
         RegionType rt = rm.getRegionType(r.getType());
         
-        //Check and remove money from the player
-        if (rt.getMoneyOutput() != 0 && HeroStronghold.econ != null) {
-            Economy econ = HeroStronghold.econ;
-            double output = rt.getMoneyOutput();
-            if (r.getOwners().isEmpty())
-                return;
-            String playername = r.getOwners().get(0);
-            if (output < 0  && econ.getBalance(playername) < Math.abs(output)) {
-                return;
-            } else if (output < 0) {
-                econ.withdrawPlayer(playername, Math.abs(output));
-            } else {
-                econ.depositPlayer(playername, output);
-            }
-        }
-        if (rt.getReagents().isEmpty() && rt.getOutput().isEmpty())
-            return;
+        
         BlockState bs = l.getBlock().getState();
-        if (!(bs instanceof Chest))
+        if (!(bs instanceof Chest)) {
             return;
+        }
         Chest chest = (Chest) bs;
         
+        
+        //Remove the upkeep items from the region chest and add items from output
+        boolean hasUpkeep = true;
+        for (ItemStack is : rt.getUpkeep()) {
+            if (chest.getInventory().contains(is)) {
+                hasUpkeep = false;
+            }
+        }
+        if (!hasUpkeep) {
+            return;
+        }
+        
+        //Check and remove money from the player
+        String playername = r.getOwners().get(0);
+        double output = rt.getMoneyOutput();
+        if (rt.getMoneyOutput() != 0 && HeroStronghold.econ != null) {
+            if (r.getOwners().isEmpty()) {
+                return;
+            }
+            if (output < 0  && HeroStronghold.econ.getBalance(playername) < Math.abs(output)) {
+                return;
+            }
+        }
+        
+        
+        for (ItemStack is : rt.getUpkeep()) {
+            chest.getInventory().removeItem(is);
+        }
         if (chest.getInventory().firstEmpty() < 0) {
             return;
         }
         
-        //Remove the upkeep items from the region chest and add items from output
-        /*for (ItemStack is : rt.getUpkeep()) {
-            if (chest.getInventory().contains(is)) {
-                chest.getInventory().removeItem(is);
+        if (HeroStronghold.econ != null) {
+            if (output < 0) {
+                HeroStronghold.econ.withdrawPlayer(playername, Math.abs(output));
+            } else {
+                HeroStronghold.econ.depositPlayer(playername, output);
             }
         }
         for (ItemStack is : rt.getOutput()) {
             chest.getInventory().addItem(is);
-        }*/
-        Map<Material, Integer> upkeepMap = new EnumMap<Material, Integer>(Material.class);
+        }
+        
+        
+        /*Map<Material, Integer> upkeepMap = new EnumMap<Material, Integer>(Material.class);
         Map<Material, Integer> outputMap = new EnumMap<Material, Integer>(Material.class);
         for (ItemStack is : rt.getUpkeep()) {
             if (is != null)
@@ -246,55 +346,73 @@ public class Effect {
                 }
             }
         }
-        chest.getInventory().setContents(is);
+        chest.getInventory().setContents(is);*/
         chest.update(true);
     }
     
+    /**
+     * Forces the region to consume upkeep items and output money and items
+     * if a random number is lower than the upkeep-chance.
+     * 
+     * @param location the location of the center of the region region.getLocation()
+     */
     public boolean upkeep(Location l) {
         RegionManager rm = getPlugin().getRegionManager();
         Region r = rm.getRegion(l);
         RegionType rt = rm.getRegionType(r.getType());
-        if (Math.random() > rt.getUpkeepChance())
+        if (Math.random() > rt.getUpkeepChance()) {
             return false;
-        
-        //Check and remove money from the player
-        if (rt.getMoneyOutput() != 0 && HeroStronghold.econ != null) {
-            Economy econ = HeroStronghold.econ;
-            double output = rt.getMoneyOutput();
-            if (r.getOwners().isEmpty())
-                return false;
-            String playername = r.getOwners().get(0);
-            if (output < 0  && econ.getBalance(playername) < Math.abs(output)) {
-                return false;
-            } else if (output < 0) {
-                econ.withdrawPlayer(playername, Math.abs(output));
-            } else {
-                econ.depositPlayer(playername, output);
-            }
         }
-        
-        if (rt.getReagents().isEmpty() && rt.getOutput().isEmpty())
-            return true;
         
         BlockState bs = l.getBlock().getState();
         if (!(bs instanceof Chest))
             return false;
         Chest chest = (Chest) bs;
         
+        //Remove the upkeep items from the region chest and add items from output
+        boolean hasUpkeep = true;
+        for (ItemStack is : rt.getUpkeep()) {
+            if (chest.getInventory().contains(is)) {
+                hasUpkeep = false;
+            }
+        }
+        if (!hasUpkeep) {
+            return false;
+        }
+        
+        
+        //Check and remove money from the player
+        String playername = r.getOwners().get(0);
+        double output = rt.getMoneyOutput();
+        if (output != 0 && HeroStronghold.econ != null) {
+            Economy econ = HeroStronghold.econ;
+            if (r.getOwners().isEmpty()) {
+                return false;
+            }
+            if (output < 0  && econ.getBalance(playername) < Math.abs(output)) {
+                return false;
+            }
+        }
+        
+        
+        for (ItemStack is : rt.getUpkeep()) {
+            chest.getInventory().removeItem(is);
+        }
         if (chest.getInventory().firstEmpty() < 0) {
             return false;
         }
         
-        //Remove the upkeep items from the region chest and add items from output
-        /*for (ItemStack is : rt.getUpkeep()) {
-            if (chest.getInventory().contains(is)) {
-                chest.getInventory().removeItem(is);
+        if (HeroStronghold.econ != null) {
+            if (output < 0) {
+                HeroStronghold.econ.withdrawPlayer(playername, Math.abs(output));
+            } else {
+                HeroStronghold.econ.depositPlayer(playername, output);
             }
         }
         for (ItemStack is : rt.getOutput()) {
             chest.getInventory().addItem(is);
-        }*/
-        Map<Material, Integer> upkeepMap = new EnumMap<Material, Integer>(Material.class);
+        }
+        /*Map<Material, Integer> upkeepMap = new EnumMap<Material, Integer>(Material.class);
         Map<Material, Integer> outputMap = new EnumMap<Material, Integer>(Material.class);
         for (ItemStack is : rt.getUpkeep()) {
             if (is != null)
@@ -351,7 +469,7 @@ public class Effect {
                 }
             }
         }
-        chest.getInventory().setContents(is);
+        chest.getInventory().setContents(is);*/
         chest.update(true);
         return true;
     }
