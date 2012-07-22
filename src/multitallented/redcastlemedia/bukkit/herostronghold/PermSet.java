97bfb9f453bca9546295ae0f5164123a3b46d28e
Multitallented <multitallented@pacbell.net>
2012-07-21 17:44:20 -0700

    Fixed the PermSet not finding region limits correctly in groups.yml

--- src/multitallented/redcastlemedia/bukkit/herostronghold/PermSet.java
+++ src/multitallented/redcastlemedia/bukkit/herostronghold/PermSet.java
@@ -7,7 +7,7 @@ package multitallented.redcastlemedia.bukkit.herostronghold;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -17,8 +17,27 @@ import org.bukkit.configuration.file.YamlConfiguration;
  * @author Multitallented
  */
 public class PermSet {
-    public HashMap<String, HashMap<String, Integer>> loadPermSets(HeroStronghold plugin) {
-        HashMap<String, HashMap<String, Integer>> tempMap = new HashMap<String, HashMap<String, Integer>>();
+    private int priority = 0;
+    private HashMap<String, Integer> perms = new HashMap<String, Integer>();
+    
+    public int getPriority() {
+        return priority;
+    }
+    
+    public void setPriority(int input) {
+        this.priority = input;
+    }
+    
+    public HashMap<String,Integer> getPerms() {
+        return perms;
+    }
+    
+    public void setPerms(HashMap<String, Integer> input) {
+        this.perms = input;
+    }
+    
+    public static HashMap<String, PermSet> loadPermSets(HeroStronghold plugin) {
+        HashMap<String, PermSet> tempMap = new HashMap<String, PermSet>();
         try {
             File groupFile = new File(plugin.getDataFolder(), "groups.yml");
             if (!groupFile.exists()) {
@@ -28,12 +47,16 @@ public class PermSet {
             FileConfiguration groupConfig = new YamlConfiguration();
             groupConfig.load(groupFile);
             for (String key : groupConfig.getKeys(false)) {
+                PermSet ps = new PermSet();
                 ConfigurationSection cs = groupConfig.getConfigurationSection(key);
                 HashMap<String, Integer> currentMap = new HashMap<String, Integer>();
-                for (String s : cs.getKeys(false)) {
-                    currentMap.put(s, cs.getInt(s));
+                ps.setPriority(groupConfig.getInt(key + ".priority", 0));
+                ConfigurationSection cs1 = cs.getConfigurationSection("limits");
+                for (String s : cs1.getKeys(false)) {
+                    currentMap.put(s, cs1.getInt(s));
                 }
-                tempMap.put(key, currentMap);
+                ps.setPerms(currentMap);
+                tempMap.put(key, ps);
             }
             
         } catch (Exception e) {
