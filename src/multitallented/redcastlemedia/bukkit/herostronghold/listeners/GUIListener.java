d5362ed448ac86ca0998ffb0f4241a0dde663b61
Multitallented <multitallented@gmail.com>
2014-08-24 15:59:03 -0700

    added correct functionality for clicking on a region

--- src/multitallented/redcastlemedia/bukkit/herostronghold/listeners/GUIListener.java
+++ src/multitallented/redcastlemedia/bukkit/herostronghold/listeners/GUIListener.java
@@ -15,10 +15,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -99,28 +96,6 @@ public class GUIListener implements Listener {
         player.openInventory(inv);
     }
     
-    private void openGUI(Player player) {
-        Inventory inv = Bukkit.createInventory(null, 9, ChatColor.DARK_RED 
-                + "HeroStronghold Regions");
-        
-        ItemStack house = new ItemStack(Material.WOOD_DOOR);
-        ItemMeta houseMeta = house.getItemMeta();
-        ItemStack cobblequery = new ItemStack(Material.COBBLESTONE);
-        ItemMeta cobblequeryMeta = cobblequery.getItemMeta();
-        
-        houseMeta.setDisplayName(ChatColor.GOLD + "House");
-        house.setItemMeta(houseMeta);
-        
-        cobblequeryMeta.setDisplayName(ChatColor.GOLD + "Cobblestone Query");
-        cobblequery.setItemMeta(cobblequeryMeta);
-        
-        // 012345678   2+6
-        inv.setItem(2, house);
-        inv.setItem(5, cobblequery);
-        
-        player.openInventory(inv);
-    }
-    
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         if (!ChatColor.stripColor(event.getInventory().getName())
@@ -133,37 +108,10 @@ public class GUIListener implements Listener {
         if(event.getCurrentItem()==null 
                 || event.getCurrentItem().getType()==Material.AIR
                 ||!event.getCurrentItem().hasItemMeta()){
-            player.closeInventory();
-            return;  
-        }
-        switch(event.getCurrentItem().getType()) {
-            case WOOD_DOOR:
-                player.closeInventory();
-                player.sendMessage(String.format("%sHeroStronghold Regions: %sHouses", 
-                        ChatColor.DARK_RED, ChatColor.GOLD));
-                break;
-            case COBBLESTONE:
-                player.closeInventory();
-                player.sendMessage(String.format("%sHeroStronghold Regions: %sCobbleQuery", 
-                        ChatColor.DARK_RED, ChatColor.GOLD));
-                break; 
-            default:
-                player.closeInventory();
-                break;
-        }
-    }
-    
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Action a = event.getAction();
-        ItemStack is = event.getItem();
-        
-        if(a == Action.PHYSICAL || is == null || is.getType()==Material.AIR) {
+            //player.closeInventory();
             return;
         }
-        
-        if(is.getType() == Material.COMPASS) {
-            openGUI(event.getPlayer());
-        }
-    } 
+        player.closeInventory();
+        player.performCommand("hs info " + ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()));
+    }
 }
