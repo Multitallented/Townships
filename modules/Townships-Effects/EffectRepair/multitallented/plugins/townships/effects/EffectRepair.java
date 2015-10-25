09848600c012b801546e36806246bf1a41f465d1
Multitallented <multitallented@gmail.com>
2015-10-24 18:05:38 -0700

    only use repair when you right click on the iron block

--- modules/Townships-Effects/EffectRepair/multitallented/plugins/townships/effects/EffectRepair.java
+++ modules/Townships-Effects/EffectRepair/multitallented/plugins/townships/effects/EffectRepair.java
@@ -13,6 +13,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -162,7 +163,7 @@ public class EffectRepair extends Effect
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event)
     {
-      if ((event.isCancelled()) || (!event.getClickedBlock().getType().equals(Material.IRON_BLOCK))) {
+      if ((event.isCancelled()) || !event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || (!event.getClickedBlock().getType().equals(Material.IRON_BLOCK))) {
         return;
       }
 
