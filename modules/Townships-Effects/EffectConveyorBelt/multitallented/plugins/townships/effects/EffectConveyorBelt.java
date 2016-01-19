e0d0e64b1698705bd20bbd69abfd83e5ab87682d
DMiller <daniel.miller@wellsource.com>
2016-01-19 15:24:18 -0800

    performance optimization for conveyors

--- modules/Townships-Effects/EffectConveyorBelt/multitallented/plugins/townships/effects/EffectConveyorBelt.java
+++ modules/Townships-Effects/EffectConveyorBelt/multitallented/plugins/townships/effects/EffectConveyorBelt.java
@@ -37,129 +37,158 @@ public class EffectConveyorBelt extends Effect {
     }
     
     public class UpkeepListener implements Listener {
-    private final EffectConveyorBelt effect;
-    private final RegionManager rm;
-    private HashMap<Region, StorageMinecart> carts = new HashMap<Region, StorageMinecart>();
-    private HashMap<Region, Location> cachePoints = new HashMap<Region, Location>();
-    private HashMap<Region, Region> cacheRegions = new HashMap<Region, Region>();
-    public UpkeepListener(EffectConveyorBelt effect, RegionManager rm) {
-        this.effect = effect;
-        this.rm = rm;
-    }
-
-    @EventHandler
-    public void onCustomEvent(ToTwoSecondEffectEvent event) {
-        if (event.getEffect().length < 2 || !event.getEffect()[0].equals("conveyor")) {
-            return;
-        }
-        Region r = event.getRegion();
-        Location l = r.getLocation();
-
-        //Check if has effect conveyor
-        RegionType rt = rm.getRegionType(r.getType());
-        if (rt == null) {
-            return;
-        }
-        int conveyor = Integer.parseInt(event.getEffect()[1]);
-        if (conveyor < 1) {
-            return;
+        private final EffectConveyorBelt effect;
+        private final RegionManager rm;
+        private HashMap<Region, StorageMinecart> carts = new HashMap<Region, StorageMinecart>();
+        private HashMap<Region, Location> cachePoints = new HashMap<Region, Location>();
+        private HashMap<Region, Region> cacheRegions = new HashMap<Region, Region>();
+        public UpkeepListener(EffectConveyorBelt effect, RegionManager rm) {
+            this.effect = effect;
+            this.rm = rm;
         }
 
-        handleExistingCarts(r);
-
-        //Check if has reagents
-        if (!effect.hasReagents(l)) {
-            return;
+        @EventHandler
+        public void onPoweredRailBreak(BlockBreakEvent event) {
+            if (event.isCancelled() || event.getBlock().getType() != Material.POWERED_RAIL) {
+                return;
+            }
+            Location location = event.getBlock().getLocation();
+            ArrayList<Region> regions = rm.getContainingBuildRegions(location);
+            if (regions.isEmpty()) {
+                return;
+            }
+            cacheRegions.remove(regions.get(0));
         }
 
-        Location loc = null;
-        if (cachePoints.containsKey(r)) {
-            loc = cachePoints.get(r);
-        } else {
-            double radius = rt.getRawBuildRadius();
-            double x0 = l.getX();
-            double y0 = l.getY();
-            double z0 = l.getZ();
-            outer: for (int x = (int) (x0 - radius); x < x0 + radius; x++) {
-                for (int y = (int) (y0 - radius); y < y0 + radius; y++) {
-                    for (int z = (int) (z0 - radius); z < z0 + radius; z++) {
-                        Block b = l.getWorld().getBlockAt(x, y, z);
-                        if (b.getType() == Material.POWERED_RAIL) {
-                            cachePoints.put(r, b.getRelative(BlockFace.UP).getLocation());
-                            break outer;
+        @EventHandler
+        public void onCustomEvent(ToTwoSecondEffectEvent event) {
+            if (event.getEffect().length < 2 || !event.getEffect()[0].equals("conveyor")) {
+                return;
+            }
+            Region r = event.getRegion();
+            Location l = r.getLocation();
+
+            //Check if has effect conveyor
+            RegionType rt = rm.getRegionType(r.getType());
+            if (rt == null) {
+                return;
+            }
+            int conveyor = Integer.parseInt(event.getEffect()[1]);
+            if (conveyor < 1) {
+                return;
+            }
+
+            handleExistingCarts(r);
+
+            //Check if has reagents
+            if (!effect.hasReagents(l)) {
+                cachePoints.remove(r);
+                cacheRegions.remove(r);
+                return;
+            }
+
+            Location loc = null;
+            if (cachePoints.containsKey(r)) {
+                loc = cachePoints.get(r);
+            } else {
+                double radius = rt.getRawBuildRadius();
+                double x0 = l.getX();
+                double y0 = l.getY();
+                double z0 = l.getZ();
+                outer: for (int x = (int) (x0 - radius); x < x0 + radius; x++) {
+                    for (int y = (int) (y0 - radius); y < y0 + radius; y++) {
+                        for (int z = (int) (z0 - radius); z < z0 + radius; z++) {
+                            Block b = l.getWorld().getBlockAt(x, y, z);
+                            if (b.getType() == Material.POWERED_RAIL) {
+                                cachePoints.put(r, b.getRelative(BlockFace.UP).getLocation());
+                                break outer;
+                            }
                         }
                     }
                 }
-            }
-            if (loc == null) {
-                return;
-            }
-            loc = cachePoints.get(r);
-        }
-
-        Chest chest = null;
-        try {
-            chest = (Chest) l.getBlock().getState();
-        } catch (Exception e) {
-            return;
-        }
-        Inventory cInv = chest.getInventory();
-        HashSet<ItemStack> iss = new HashSet<ItemStack>();
-        if (!cInv.contains(Material.STORAGE_MINECART) || !cInv.contains(conveyor)) {
-            return;
-        }
-        for (ItemStack is : cInv.getContents()) {
-            if (is != null && is.getTypeId() == conveyor) {
-                iss.add(is);
-            }
-        }
-        if (iss.isEmpty()) {
-            return;
-        }
-
-        //If chunk not loaded try using region cache to move directly
-        if (!loc.getChunk().isLoaded()) {
-            if (cacheRegions.containsKey(r)) {
-                Chest tempChest = (Chest) cacheRegions.get(r).getLocation().getBlock().getState();
-                if (tempChest.getInventory().firstEmpty() < 0) {
+                if (loc == null) {
                     return;
                 }
+                loc = cachePoints.get(r);
+            }
+
+            Chest chest = null;
+            try {
+                chest = (Chest) l.getBlock().getState();
+            } catch (Exception e) {
+                return;
+            }
+            Inventory cInv = chest.getInventory();
+            HashSet<ItemStack> iss = new HashSet<ItemStack>();
+            if (!cInv.contains(Material.STORAGE_MINECART) || !cInv.contains(conveyor)) {
+                return;
+            }
+            for (ItemStack is : cInv.getContents()) {
+                if (is != null && is.getTypeId() == conveyor) {
+                    iss.add(is);
+                }
+            }
+            if (iss.isEmpty()) {
+                return;
+            }
+
+            //If chunk not loaded try using region cache to move directly
+            if (!loc.getChunk().isLoaded()) {
+                if (cacheRegions.containsKey(r)) {
+                    Chest tempChest = (Chest) cacheRegions.get(r).getLocation().getBlock().getState();
+                    if (tempChest.getInventory().firstEmpty() < 0) {
+                        return;
+                    }
+                    for (ItemStack is : iss) {
+                        cInv.removeItem(is);
+                    }
+                    try {
+                        for (ItemStack is : iss) {
+                            tempChest.getInventory().addItem(is);
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+                return;
+            } else {
                 for (ItemStack is : iss) {
                     cInv.removeItem(is);
                 }
-                try {
-                    for (ItemStack is : iss) {
-                        tempChest.getInventory().addItem(is);
-                    }
-                } catch (Exception e) {
 
+                ItemStack tempCart = new ItemStack(Material.STORAGE_MINECART, 1);
+                cInv.removeItem(tempCart);
+
+                StorageMinecart cart = loc.getWorld().spawn(loc, StorageMinecart.class);
+
+                for (ItemStack is : iss) {
+                    cart.getInventory().addItem(is);
                 }
+                carts.put(r, cart);
             }
-            return;
-        } else {
-            for (ItemStack is : iss) {
-                cInv.removeItem(is);
-            }
-
-            ItemStack tempCart = new ItemStack(Material.STORAGE_MINECART, 1);
-            cInv.removeItem(tempCart);
-
-            StorageMinecart cart = loc.getWorld().spawn(loc, StorageMinecart.class);
-
-            for (ItemStack is : iss) {
-                cart.getInventory().addItem(is);
-            }
-            carts.put(r, cart);
         }
-    }
 
-    private void handleExistingCarts(Region r) {
+        private void handleExistingCarts(Region r) {
             HashSet<Region> removeMe = new HashSet<Region>();
 
             if (carts.containsKey(r)) {
                 StorageMinecart sm = carts.get(r);
-                if (sm.isDead()) {
-                    removeMe.add(r);
+                if (!sm.isValid() || sm.isDead()) {
+                    carts.remove(r);
+                    return;
+                }
+                if (!sm.getLocation().getChunk().isLoaded()) {
+                    boolean cartRemoved = false;
+                    try {
+                        Chest returnChest = (Chest) r.getLocation().getBlock().getState();
+                        cartRemoved = returnChest.getInventory().addItem(new ItemStack(Material.STORAGE_MINECART, 1));
+                    } catch (Exception e) {
+                        //don't care
+                    }
+                    if (cartRemoved) {
+                        sm.remove();
+                    }
+                    carts.remove(r);
                     return;
                 }
                 ArrayList<Region> regions = rm.getContainingBuildRegions(sm.getLocation());
