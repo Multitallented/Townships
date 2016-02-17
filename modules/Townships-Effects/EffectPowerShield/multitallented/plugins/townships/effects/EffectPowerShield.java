a640f95b448c922218e36aadd11e8fc4c22ea50e
DMiller <daniel.miller@wellsource.com>
2016-02-17 14:58:18 -0800

    work in progress on power_shield

--- modules/Townships-Effects/EffectPowerShield/multitallented/plugins/townships/effects/EffectPowerShield.java
+++ modules/Townships-Effects/EffectPowerShield/multitallented/plugins/townships/effects/EffectPowerShield.java
@@ -43,7 +43,32 @@ public class EffectPowerShield extends Effect {
             if (!(e instanceof TNTPrimed)) {
                 return;
             }
-            if (rm.shouldTakeAction(event.getLocation(), null, new RegionCondition("power_shield", true, 0))) {
+
+            //TODO offset this by 5
+            superRegionLoop: for (SuperRegion sr : rm.getContainingSuperRegions(event.getLocation())) {
+                int powerDamage = 1;
+                for (String effectString : rm.getSuperRegionType(sr.getType()).getEffects()) {
+                    if (effectString.startsWith("power_shield")) {
+                        String[] effectParts = effectString.split("\\.");
+                        if (effectParts.length > 1) {
+                            try {
+                                powerDamage = Integer.parseInt(effectParts[1]);
+                            } catch (Exception e) {
+
+                            }
+                        }
+                        if (sr.getPower() > 0) {
+                            event.setCancelled(true);
+                            sr.setPower(Math.max(0, sr.getPower() - powerDamage));
+                            //TODO check for destroy region
+                            break superRegionLoop;
+                        }
+                    }
+                }
+            }
+
+
+            /*if (rm.shouldTakeAction(event.getLocation(), null, new RegionCondition("power_shield", true, 0))) {
                 boolean powerReduced = false;
                 for (SuperRegion sr : rm.getContainingSuperRegions(event.getLocation())) {
                     if (sr.getPower() > 0 && rm.getSuperRegionType(sr.getType()).getEffects().contains("power_shield")) {
@@ -55,7 +80,7 @@ public class EffectPowerShield extends Effect {
                 if (powerReduced) {
                     event.setCancelled(true);
                 }
-            }
+            }*/
         }
     }
 }
